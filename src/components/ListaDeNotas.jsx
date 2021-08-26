8281e149e4eb42de58c8ea17977a9a45e51095e6
João Dias <joaov.oliveiradias@gmail.com>
2021-08-26 17:08:08 -0300

    Initial commit

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import CardNota from "./CardNota";
+
+export default class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria,index) => {
+          return (
+            <li key={index}>
+              <div>{categoria}</div>
+              <CardNota />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
